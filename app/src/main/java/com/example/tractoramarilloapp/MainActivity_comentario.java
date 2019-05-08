2c779f81669caed570c2c9f2913221cde2277c13
Ricardo Etcheverry <retcheverry@angular.cl>
2019-05-08 14:27:36 -0400

    Block read NFC in comment activity

--- app/src/main/java/com/example/tractoramarilloapp/MainActivity_comentario.java
+++ app/src/main/java/com/example/tractoramarilloapp/MainActivity_comentario.java
@@ -1,16 +1,22 @@
 package com.example.tractoramarilloapp;
 
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.tractoramarilloapp.model.Comentarios;
+import com.example.tractoramarilloapp.nfc.NFCHandler;
 import com.example.tractoramarilloapp.persistence.HandlerDBPersistence;
 
 import java.text.SimpleDateFormat;
@@ -20,6 +26,16 @@ public class MainActivity_comentario extends AppCompatActivity {
 
     private Button acceptButton,cancelButton;
     private EditText comentarioField;
+
+    NFCHandler nfcHandler;
+    NfcAdapter nfcAdapter;
+    PendingIntent pendingIntent;
+    IntentFilter writeTagFilters[];
+    Tag myTag;
+    Context context;
+    TextView tvNFCContent;
+    TextView message;
+    Button btnWrite;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,32 +52,44 @@ public class MainActivity_comentario extends AppCompatActivity {
         cancelButton = (Button) findViewById(R.id.buttonCancel);
         comentarioField = (EditText) findViewById(R.id.editTextComentarios);
 
+        // NFC CONFIGURATION
+        context = this;
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+
+        pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+        IntentFilter tagDetected = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
+        tagDetected.addCategory(Intent.CATEGORY_DEFAULT);
+        writeTagFilters = new IntentFilter[] { tagDetected };
+        this.nfcHandler = new NFCHandler(this, context, nfcAdapter);
+
+        //instanciamos al handler de
+        String text = this.nfcHandler.readerTAGNFC(getIntent());
 
         acceptButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                //obtenemos la informacion para almacenar el comentario en la BD
-                SharedPreferences prefs = getSharedPreferences("MisPreferencias", Context.MODE_PRIVATE);
-                String idUser = prefs.getString("idUsuario", "null");
+            //obtenemos la informacion para almacenar el comentario en la BD
+            SharedPreferences prefs = getSharedPreferences("MisPreferencias", Context.MODE_PRIVATE);
+            String idUser = prefs.getString("idUsuario", "null");
 
-                HandlerDBPersistence handlerDBPersistence = new HandlerDBPersistence(getApplicationContext());
+            HandlerDBPersistence handlerDBPersistence = new HandlerDBPersistence(getApplicationContext());
 
-                String sqlQueryInforme = "SELECT * FROM comentario";
-                int lastID = handlerDBPersistence.getLastID(sqlQueryInforme, "idComentario");
+            String sqlQueryInforme = "SELECT * FROM comentario";
+            int lastID = handlerDBPersistence.getLastID(sqlQueryInforme, "idComentario");
 
-                if (lastID == -1){
-                    lastID = 1;
-                }else{
-                    lastID++;
-                }
-                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-                String currentDateandTime = sdf.format(new Date());
-                handlerDBPersistence.saveComentario(new Comentarios(lastID, idUser,  currentDateandTime, comentarioField.getText().toString()));
-                Intent output = new Intent();
-                output.putExtra("comentario", comentarioField.getText().toString());
-                setResult(RESULT_OK, output);
-                finish();
+            if (lastID == -1){
+                lastID = 1;
+            }else{
+                lastID++;
+            }
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            String currentDateandTime = sdf.format(new Date());
+            handlerDBPersistence.saveComentario(new Comentarios(lastID, idUser,  currentDateandTime, comentarioField.getText().toString()));
+            Intent output = new Intent();
+            output.putExtra("comentario", comentarioField.getText().toString());
+            setResult(RESULT_OK, output);
+            finish();
             }
         });
 
@@ -77,15 +105,23 @@ public class MainActivity_comentario extends AppCompatActivity {
 
 
 
-
-
-
-
-
     }
 
     @Override
     public void onBackPressed() {
 
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        this.nfcHandler.changeModeWrite(0, pendingIntent, writeTagFilters);//desactivamos
+
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        this.nfcHandler.changeModeWrite(1, pendingIntent, writeTagFilters);//activamos
     }
 }
